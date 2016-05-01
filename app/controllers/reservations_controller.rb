b511bd8ad9f5aeb45d815f55d8ab47054b729cb5
Cameron Aziz <cameronaziz@gmail.com>
2016-05-01 14:15:19 -0700

    Create reservations storage

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,13 +3,28 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.new
   end
 
+  def front_new
+    @reservation = Reservation.new
+  end
+
   def edit
   end
 
   def create
+    @reservation = Reservation.new(reservation_params)
+    #todo: change to dynamic customer id
+    @reservation.customer_id = 1
+    if @reservation.save
+      redirect_to reservations_path
+    else
+      render 'reservations/new'
+    end
   end
 
+
+
   def index
+    @reservations = Reservation.all
   end
 
   def show
@@ -19,5 +34,14 @@ class ReservationsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def reservation_params
+    if params[:reservation]
+      params.require(:reservation).permit(:name, :customer_id)
+    else
+      params.permit(:name, :customer_id)
+    end
   end
 end
