149f26a172855bf73b3efe3d0488d7755a620659
Cameron Aziz <cameronaziz@gmail.com>
2016-05-01 12:58:37 -0700

    Add simple Reservations controller

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,23 @@
+class ReservationsController < ApplicationController
+  def new
+    @reservation = Reservation.new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ db/migrate/20160501194355_create_reservations.rb
@@ -0,0 +1,12 @@
+class CreateReservations < ActiveRecord::Migration
+  def change
+    create_table :reservations do |t|
+      t.belongs_to :customer
+      t.string :name
+      t.string :note
+      t.integer :party_size
+
+      t.timestamps null: false
+    end
+  end
+end
