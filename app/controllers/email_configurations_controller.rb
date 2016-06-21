a4d32a9359d758e1d762d20d5dfe1e7fd2676882
cameronaziz <cameronaziz@gmail.com>
2016-06-20 23:52:21 -0700

    email

--- app/controllers/email_configurations_controller.rb
+++ app/controllers/email_configurations_controller.rb
@@ -3,6 +3,9 @@ class EmailConfigurationsController < ApplicationController
 
   def index
     @email_configurations = EmailConfiguration.where(email_template_id: params[:id])
+    if params[:id] == 3
+      redirect_to 'email_configurations/3'
+    end
   end
 
   def show
