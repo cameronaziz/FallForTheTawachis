a9018045bcade76d001f57035458e6dafd19dcbf
cameronaziz <cameronaziz@gmail.com>
2016-06-21 13:12:05 -0700

    dynamic url in from email

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -4,9 +4,16 @@ class Mailer < ApplicationMailer
     @email_configurations = EmailConfiguration.where(email_template_id: email)
     file = File.join(Rails.root, 'app', 'assets', 'images', 'emails', '3', 'image.jpg')
     attachments.inline['image.jpg'] = File.read(file)
+
+    if customer.url == 'localhost:3000'
+      url = 'fallforthetawachis.com'
+    else
+      url = customer.url
+    end
+
     mail(to: reservation.email,
-         #from: "#{customer.from_email}@#{customer.url}",
-         from: 'mail@fallforthetawachis.com',
+         from: "#{customer.from_email}@#{url}",
+         #from: 'mail@fallforthetawachis.com',
          subject: @email_configurations.where(field_id: 1).first.value,
          template_path: 'mailer',
          template_name: email)
