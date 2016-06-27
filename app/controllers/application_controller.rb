5498289efdf306f3b77d5f49136cdb5736254f30
cameronaziz <cameronaziz@gmail.com>
2016-06-26 18:11:51 -0700

    random little fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-
+  def authenticate_user(user, customer_id)
+    user_object = User.find(user)
+    if user_object.is_administrator
+      true
+    else
+      if customer_id == user_object.customer_id
+        true
+      else
+        redirect_to :root
+      end
+    end
+  end
 
 end
