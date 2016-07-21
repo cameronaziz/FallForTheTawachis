2b440909709092d2fa8fd94d5e17909124124891
cameronaziz <cameronaziz@gmail.com>
2016-07-20 21:20:15 -0700

    Clean Up URLs

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,6 +4,8 @@ class Reservation < ActiveRecord::Base
   accepts_nested_attributes_for :persons, reject_if: lambda { |a| a[:first_name].blank? }, allow_destroy: true
   belongs_to :group
 
+  before_save :clean_url
+
   validates :name, presence: true
   validates_uniqueness_of :custom_name, :scope => [:customer_id]
 
@@ -11,6 +13,11 @@ class Reservation < ActiveRecord::Base
   def registration_time
     format = '%m/%d/%Y'
     created_at.strftime(format)
+  end
+
+  def clean_url
+    self.custom_name.downcase!
+    self.custom_name = self.custom_name.gsub(/\s+/, '')
   end
 
   def party_size_filter
